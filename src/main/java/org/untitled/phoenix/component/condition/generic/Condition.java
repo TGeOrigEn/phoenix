0e08cef4d4ae05c9f640a7f5999320d6c255e5ba
Тисло Артём Максимович <atislo@gemsdev.com>
2022-11-08 10:30:37 +0600

    12312

--- src/main/java/org/untitled/phoenix/component/condition/generic/Condition.java
+++ src/main/java/org/untitled/phoenix/component/condition/generic/Condition.java
@@ -6,12 +6,21 @@ import org.untitled.phoenix.component.Component;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * <p>Представляет собой условие, позволяющее проверить, выполняет ли компонент условие.</p>
+ * @param <TComponent> класс компонента, который выполняет данное условие
+ */
 public final class Condition<TComponent extends Component> extends BaseCondition {
 
     private final @NotNull BaseRequirement<TComponent> requirement;
 
     private final @NotNull TComponent component;
 
+    /**
+     * <p>Создаёт новый экземпляр условия в соответствии с указанным требованием для указанного компонента.</p>
+     * @param component компонент, который будет выполнять условие
+     * @param requirement требование, которое описывает, как выполнять условие компоненту
+     */
     public Condition(@NotNull TComponent component, @NotNull BaseRequirement<TComponent> requirement) {
         this.requirement = requirement;
         this.component = component;
