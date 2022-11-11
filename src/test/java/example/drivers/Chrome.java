18dbb6b56a547f8f957110eca981892694e345cd
Тисло Артём Максимович <atislo@gemsdev.com>
2022-11-12 01:03:57 +0600

    15124

--- src/test/java/example/drivers/Chrome.java
+++ src/test/java/example/drivers/Chrome.java
@@ -11,9 +11,19 @@ public final class Chrome {
     public static @NotNull ChromeOptions getDefaultOptions(@NotNull Duration timeout) {
         final var chromeOptions = new ChromeOptions();
 
-        chromeOptions.setCapability("sessionTimeout", String.format("%dms", timeout.toMillis()));
-        chromeOptions.addArguments("--browser.download.folderList=2");
         chromeOptions.setCapability("browser.download.manager.showWhenStarting", false);
+        chromeOptions.setCapability("browser.helperApps.neverAsk.openFile",
+                "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml");
+        chromeOptions.setCapability("browser.helperApps.neverAsk.saveToDisk",
+                "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml");
+        chromeOptions.setCapability("browser.helperApps.alwaysAsk.force", false);
+        chromeOptions.setCapability("browser.download.manager.alertOnEXEOpen", false);
+        chromeOptions.setCapability("browser.download.manager.focusWhenStarting", false);
+        chromeOptions.setCapability("browser.download.manager.useWindow", false);
+        chromeOptions.setCapability("browser.download.manager.showAlertOnComplete", false);
+        chromeOptions.setCapability("browser.download.manager.closeWhenDone", false);
+
+        chromeOptions.setCapability("sessionTimeout", String.format("%dms", timeout.toMillis()));
         chromeOptions.setCapability("screenResolution", "1920x1080x24");
         chromeOptions.setCapability("browserName", "chrome");
         chromeOptions.setCapability("enableVideo", true);
@@ -29,8 +39,19 @@ public final class Chrome {
 
         chromeOptions.addExtensions(new File("/opt/cades_plugin.crx"));
 
+        chromeOptions.setCapability("browser.download.manager.showWhenStarting", false);
+        chromeOptions.setCapability("browser.helperApps.neverAsk.openFile",
+                "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml");
+        chromeOptions.setCapability("browser.helperApps.neverAsk.saveToDisk",
+                "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml");
+        chromeOptions.setCapability("browser.helperApps.alwaysAsk.force", false);
+        chromeOptions.setCapability("browser.download.manager.alertOnEXEOpen", false);
+        chromeOptions.setCapability("browser.download.manager.focusWhenStarting", false);
+        chromeOptions.setCapability("browser.download.manager.useWindow", false);
+        chromeOptions.setCapability("browser.download.manager.showAlertOnComplete", false);
+        chromeOptions.setCapability("browser.download.manager.closeWhenDone", false);
+
         chromeOptions.setCapability("sessionTimeout", String.format("%dms", timeout.toMillis()));
-        chromeOptions.addArguments("--browser.download.folderList=2");
         chromeOptions.setCapability("screenResolution", "1920x1080x24");
         chromeOptions.setCapability("version", "cryptochrome");
         chromeOptions.setCapability("browserName", "chrome");
