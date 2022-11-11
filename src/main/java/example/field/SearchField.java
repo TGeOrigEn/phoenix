b9a25223814f45a3c6749cd7939d9860c402ed9e
Тисло Артём Максимович <atislo@gemsdev.com>
2022-11-12 00:24:32 +0600

    15124

--- src/main/java/example/field/SearchField.java
+++ src/main/java/example/field/SearchField.java
@@ -8,18 +8,35 @@ import org.untitled.phoenix.component.Description;
 
 public class SearchField extends Field {
 
+    private static final @NotNull Description CLEAR_BUTTON_DESCRIPTION = new Description(By.cssSelector("div[class*='x-form-clear-trigger']"), "Кнопка 'Отчистить'");
+
+    private static final @NotNull Description SEARCH_BUTTON_DESCRIPTION = new Description(By.cssSelector("div[class*='x-form-search-trigger']"), "Кнопка 'Поиск'");
+
     public static final @NotNull Description INPUT_DESCRIPTION = new Description(By.tagName("input"), "Ввод");
 
-    private static final @NotNull Description ARROW_BUTTON_DESCRIPTION = new Description(By.cssSelector("div[class*='x-form-arrow-trigger']"), "Кнопка 'Стрелка'");
+    private final @NotNull Component searchButton;
+
+    private final @NotNull Component clearButton;
 
     private final @NotNull Component input;
 
     public SearchField() {
+        searchButton = findInside(() -> new WebComponent(SEARCH_BUTTON_DESCRIPTION));
+        clearButton = findInside(() -> new WebComponent(CLEAR_BUTTON_DESCRIPTION));
         input = findInside(() -> new WebComponent(INPUT_DESCRIPTION));
     }
 
     @Override
     protected @NotNull Component getInput() {
         return input;
+    }
+
+    @Override
+    public void setValue(@NotNull String value) {
+        if (clearButton.isAvailable())
+            clearButton.toAction().click();
+
+        input.toAction().sendKeys(value);
+        searchButton.toAction().click();
     }
 }
