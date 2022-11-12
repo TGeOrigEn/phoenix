0d5ed1c5c67a7cca3d847eea8162cad7ab64918f
Тисло Артём Максимович <atislo@gemsdev.com>
2022-11-12 15:06:57 +0600

    15124

--- src/test/java/example/drivers/Chrome.java
+++ src/test/java/example/drivers/Chrome.java
@@ -11,17 +11,6 @@ public final class Chrome {
     public static @NotNull ChromeOptions getDefaultOptions(@NotNull Duration timeout) {
         final var chromeOptions = new ChromeOptions();
 
-        chromeOptions.setCapability("browser.download.folderList", 2);
-        chromeOptions.setCapability("browser.download.manager.showWhenStarting", false);
-        chromeOptions.setCapability("browser.helperApps.alwaysAsk.force", false);
-        chromeOptions.setCapability("browser.helperApps.neverAsk.saveToDisk", "*.*");
-        chromeOptions.setCapability("browser.download.manager.focusWhenStarting",false);
-        chromeOptions.setCapability("browser.download.manager.useWindow", false);
-        chromeOptions.setCapability("browser.download.manager.showAlertOnComplete", false);
-        chromeOptions.setCapability("pdfjs.disabled", true);
-
-
-
         chromeOptions.setCapability("sessionTimeout", String.format("%dms", timeout.toMillis()));
         chromeOptions.setCapability("screenResolution", "1920x1080x24");
         chromeOptions.setCapability("browserName", "chrome");
@@ -37,18 +26,6 @@ public final class Chrome {
         final var chromeOptions = new ChromeOptions();
 
         chromeOptions.addExtensions(new File("/opt/cades_plugin.crx"));
-
-        chromeOptions.addArguments("--browser.download.folderList=1");
-        chromeOptions.addArguments("--browser.download.manager.showWhenStarting=false");
-        chromeOptions.addArguments("--browser.download.manager.focusWhenStarting=false");
-        chromeOptions.addArguments("--browser.download.useDownloadDir=true");
-        chromeOptions.addArguments("--browser.helperApps.alwaysAsk.force=false");
-        chromeOptions.addArguments("--browser.download.manager.alertOnEXEOpen=false");
-        chromeOptions.addArguments("--browser.download.manager.closeWhenDone=true");
-        chromeOptions.addArguments("--browser.download.manager.showAlertOnComplete=false");
-        chromeOptions.addArguments("--browser.download.manager.useWindow=false");
-        // You will need to find the content-type of your app and set it here.
-        chromeOptions.addArguments("--browser.helperApps.neverAsk.saveToDisk=application/octet-stream");
 
         chromeOptions.setCapability("sessionTimeout", String.format("%dms", timeout.toMillis()));
         chromeOptions.setCapability("screenResolution", "1920x1080x24");
--- src/test/java/example/graphicReport/GraphicReportTest.java
+++ src/test/java/example/graphicReport/GraphicReportTest.java
@@ -65,7 +65,13 @@ public class GraphicReportTest extends BaseTest {
         Component.find(DropdownField::new, Field.Requirements.Equals.byTitle("Выберите шаблон:")).setValue("А4, Альбомный (ODG)");
         Component.find(Button::new, Button.Requirements.Equals.byText("Далее")).click();
         Component.find(DropdownField::new, Field.Requirements.Equals.byTitle("Карта:")).setValue("~1:210000");
-        Component.find(Button::new, Button.Requirements.Equals.byText("Сформировать")).download(Duration.ofSeconds(10), 1);
+        Component.find(Button::new, Button.Requirements.Equals.byText("Сформировать")).click();
+
+        Component.find(Button::new, Button.Requirements.Equals.byTip("Графический отчет")).click();
+        Component.find(DropdownField::new, Field.Requirements.Equals.byTitle("Выберите шаблон:")).setValue("А4, Альбомный (ODG)");
+        Component.find(Button::new, Button.Requirements.Equals.byText("Далее")).click();
+        Component.find(DropdownField::new, Field.Requirements.Equals.byTitle("Карта:")).setValue("~1:210000");
+        Component.find(Button::new, Button.Requirements.Equals.byText("Сформировать")).download(Duration.ofSeconds(15), 2);
     }
 
     @Test
