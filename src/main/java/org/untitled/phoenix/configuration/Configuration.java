6141a9cde762e55d4f412578945e0aa084742ef6
Тисло Артём Максимович <atislo@gemsdev.com>
2022-11-12 14:26:23 +0600

    15124

--- src/main/java/org/untitled/phoenix/configuration/Configuration.java
+++ src/main/java/org/untitled/phoenix/configuration/Configuration.java
@@ -49,6 +49,7 @@ public final class Configuration {
         final var prefs = new HashMap<String, Object>();
         prefs.put("download.default_directory", Configuration.downloadDirectory);
         prefs.put("download.prompt_for_download", false);
+        prefs.put("download.directory_upgrade", true);
         prefs.put("profile.content_settings.exceptions.automatic_downloads.*.setting", 1 );
         options.setExperimentalOption("prefs", prefs);
 
