f3efbb414ba5e1eae6f063f7c19432c80694ff48
Тисло Артём Максимович <atislo@gemsdev.com>
2022-11-25 12:36:22 +0600

    sas

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -84,7 +84,7 @@ public abstract class BaseTest {
     }
 
     protected @Nullable String initializeSelenoid() {
-        return Selenoid.NONE;
+        return Selenoid.ARTEM;
     }
 
     @BeforeEach
